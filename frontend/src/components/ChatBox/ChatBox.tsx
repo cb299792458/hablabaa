2fa1d94b6fe608b8eda09d941a93c9b100026caa
Brian Lam <brianrlam@gmail.com>
2024-06-27 11:32:03 -0400

    modify prompt

--- frontend/src/components/ChatBox/ChatBox.tsx
+++ frontend/src/components/ChatBox/ChatBox.tsx
@@ -130,8 +130,8 @@ const ChatBox: React.FC = () => {
             const systemMessage: ChatCompletionMessageParam[] = [
                 {
                     role: "system",
-                    content: `Your name is ${botName} and you are talking to your friend named ${userName} who wants to practice their ${practiceLanguage}. 
-                    You can talk to them in ${practiceLanguage}, but their preferred language is ${preferredLanguage}, which you can also use.`,
+                    content: `Your name is ${botName} and you are talking to your friend named ${userName}, who wants to practice their ${practiceLanguage}. 
+                    You can talk to them in ${practiceLanguage}, but their preferred language is ${preferredLanguage}, which you should use if their message doesn't make any sense.`,
                 }
             ]
             const pastMessages: ChatCompletionMessageParam[] = messages.map((message) => ({
